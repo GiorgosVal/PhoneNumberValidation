554c949d9c064eebb72f07df5673487bc79c906e
Giorgos Valamatsas <giorgos.valamats@gmail.com>
2019-09-13 15:37:41 +0300

    validation and initialization of input

--- src/phonenumbervalidation/PhoneNumberValidation.java
+++ src/phonenumbervalidation/PhoneNumberValidation.java
@@ -5,17 +5,48 @@
  */
 package phonenumbervalidation;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author giorgos
  */
 public class PhoneNumberValidation {
 
+    public String[] input;
+    public Map<String, List<Integer>> interpretations;
+
+    public boolean validateAndInitializeInput(String in) {
+        String[] temp = in.split(" ");
+        for (String s : temp) {
+            try {
+                if (s.length() > 3 || Integer.parseInt(s) < 0) {
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        input = temp;
+        return true;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        String s = "0 0 30 69 700 24 1 3 50 2";
+
+        PhoneNumberValidation app = new PhoneNumberValidation();
+
+        boolean a = app.validateAndInitializeInput(s);
+        System.out.println(a);
+        
+
+        
     }
-    
+
 }
